a274fa1290e1930a37998313a1d66e52b1511d41
carloshlweber <carloshlweber@gmail.com>
2026-09-10 17:32:25 -0300

    chore: initialize project structure and application entrypoint #1

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from presentation.app import PipelineApp
+
+
+def main():
+    app = PipelineApp()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/presentation/app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+
+class PipelineApp:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Pipeline de Boletos")
+
+    def run(self):
+        self.root.mainloop()
